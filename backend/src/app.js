47bc1d812e8fddfb6b706774efc2b8a85c955eda
QuytChua <22110261@student.hcmute.edu.vn>
2026-07-16 13:30:59 +0700

    setup be

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Booking Care API is running",
+  });
+});
+
+export default app;
